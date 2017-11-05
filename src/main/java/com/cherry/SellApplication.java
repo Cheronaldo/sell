3d942f1b17c1f20a48e96ddc44e61349f23e4958
Cheronaldo <601585410@qq.com>
2017-11-05 08:54:34 +0800

    sell

--- src/main/java/com/cherry/SellApplication.java
+++ src/main/java/com/cherry/SellApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 //mybatis 注解方式（或者配置方式）的 mapper接口类的扫描路径配置  这样mapper接口中就不用添加@Mapper注解
-
+//@EnableCaching 用于redis缓存
 @SpringBootApplication
 @MapperScan(basePackages = "com.cherry.dataobject.mapper")
 @EnableCaching
